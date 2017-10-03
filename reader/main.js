fa2227aa1a181848e970623602c81c3411ad2604
msramalho <up201403027@fe.up.pt>
2017-10-03 17:30:59 +0100

    fixed main.js serial include of primitives

--- reader/main.js
+++ reader/main.js
@@ -13,7 +13,7 @@ function getUrlVars() {
 
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 
              'MyGraphNode.js', 'MyGraphLeaf.js', 'MyInterface.js',
-             'Rectangle.js', 'Triangle.js', 'Sphere.js', 'Cylinder.js',
+             'Primitives/Rectangle.js', 'Primitives/Triangle.js', 'Primitives/Sphere.js', 'Primitives/Cylinder.js',
 
 main=function()
 {
