884a069a4d0e1d8fd80a40b8350f7b48ad7925c7
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-10-03 16:57:39 +0100

    Primitives interpretation

--- reader/MyGraphLeaf.js
+++ reader/MyGraphLeaf.js
@@ -1,20 +1,26 @@
 /**
  * MyGraphLeaf class, representing a leaf in the scene graph.
  * @constructor
-**/
+ **/
 
 function MyGraphLeaf(graph, xmlelem) {
     this.graph = graph;
     this.element = xmlelem;
 
-    this.type=this.graph.reader.getItem(this.element, 'type', ['rectangle', 'cylinder', 'sphere', 'triangle']);
-    this.args = this.graph.reader.getString(this.element, 'args').split(" ");
+    this.type = this.graph.reader.getItem(this.element, 'type', ['rectangle', 'cylinder', 'sphere', 'triangle']);
+    this.args = this.graph.reader.getString(this.element, 'args').split(" ").map(function (a) { parseFloat(a) });
     this.primitive = null;
     console.log(this.type);
-    
-    if (this.type == "rectangle"){
-        
+
+    if (this.type == "rectangle") {
+        this.primitive = new Rectangle(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3]);
+    }/* else if (this.type == "triangle") {
+        this.primitive = new Triangle(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
+    } else if (this.type == "sphere") {
+        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
+    } else if (this.type == "sphere") {
+        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
     }
-    
+    */
 }
 
