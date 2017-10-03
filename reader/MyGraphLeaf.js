e3ca4e18084199321c36c4b0fdb4d5fc7d034067
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-10-03 17:41:27 +0100

    Primitives done

--- reader/MyGraphLeaf.js
+++ reader/MyGraphLeaf.js
@@ -14,13 +14,12 @@ function MyGraphLeaf(graph, xmlelem) {
 
     if (this.type == "rectangle") {
         this.primitive = new Rectangle(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3]);
-    }/* else if (this.type == "triangle") {
+    } else if (this.type == "triangle") {
         this.primitive = new Triangle(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
     } else if (this.type == "sphere") {
-        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
-    } else if (this.type == "sphere") {
-        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4], this.args[5], this.args[6], this.args[7], this.args[8]);
+        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2]);
+    } else if (this.type == "cylinder") {
+        this.primitive = new Sphere(this.graph.scene, this.args[0], this.args[1], this.args[2], this.args[3], this.args[4]);
     }
-    */
 }
 
