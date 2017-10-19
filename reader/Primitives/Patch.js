4946973419faae699ae0b241407131b1c5ad04ed
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-10-19 15:24:58 +0100

    Performance Improvements and Bridge end

--- reader/Primitives/Patch.js
+++ reader/Primitives/Patch.js
@@ -3,10 +3,10 @@ function Patch(scene, div_u, div_v, cpoints) {
     this.scene = scene;
     this.div_u = div_u;
     this.div_v = div_v;
-    var degree_1 = cpoints.length - 1;
-    var degree_2 = cpoints[0].length - 1;
+    var degree1 = cpoints.length - 1;
+    var degree2 = cpoints[0].length - 1;
 
-    this.makeSurface(degree_1, degree_2, cpoints);
+    this.makeSurface(degree1, degree2, cpoints);
 }
 
 Patch.prototype = Object.create(CGFnurbsObject.prototype);
@@ -19,12 +19,12 @@ Patch.prototype.getKnotsVector = function(degree) {
     return array0.concat(array1);
 }
 
-Patch.prototype.makeSurface = function(degree_1, degree_2, cpoints) {
+Patch.prototype.makeSurface = function(degree1, degree2, cpoints) {
     //create the nurbs surface from the knots and degress
-    var knots_1 = this.getKnotsVector(degree_1);
-    var knots_2 = this.getKnotsVector(degree_2);
+    var knots_1 = this.getKnotsVector(degree1);
+    var knots_2 = this.getKnotsVector(degree2);
 
-    var nurbs_surface = new CGFnurbsSurface(degree_1, degree_2, knots_1, knots_2, cpoints);
+    var nurbs_surface = new CGFnurbsSurface(degree1, degree2, knots_1, knots_2, cpoints);
 
     getSurfacePoint = function(u, v) {
         return nurbs_surface.getPoint(u, v);
@@ -36,9 +36,6 @@ Patch.prototype.makeSurface = function(degree_1, degree_2, cpoints) {
 Patch.prototype.display = function() {
     this.nurbs_object.display();
 }
-
-//function stubs so the MySceneGraph can invoke them on any nodes
-Patch.prototype.updateTexture = function(texture) {}
 
 //function stubs so the MySceneGraph can invoke them on any nodes
 Patch.prototype.setAmplifFactor = function(amplif_s, amplif_t) {}
