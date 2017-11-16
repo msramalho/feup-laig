356affd7509f069a8c2e867f57a5470569a8e016
Afonso Jorge Ramos <up201506239@fe.up.pt>
2017-11-16 18:01:36 +0000

    Fixed Circular Animation

--- reader/Animations/CircularAnimation.js
+++ reader/Animations/CircularAnimation.js
@@ -1,6 +1,8 @@
-function CircularAnimation(speed, center, radius, startAng, rotAng) {
+function CircularAnimation(speed, centerx, centery, centerz, radius, startAng, rotAng) {
 	this.speed = speed;
-	this.center = center;
+	this.centerx = centerx;
+	this.centery = centery;
+	this.centerz = centerz;
 	this.radius = radius;
 	this.startAng = startAng;
 	this.rotAng = rotAng;
@@ -15,7 +17,7 @@ CircularAnimation.prototype.animate = function(time) {
 	if (time > this.speed)
 		time = this.speed;
 
-	mat4.translate(matrix, matrix, this.center);
+	mat4.translate(matrix, matrix, [this.centerx, this.centery, this.centerz]);
 	mat4.rotate(matrix, matrix, this.startAng + (time / this.speed) * this.rotAng, [0, 1, 0]);
 	mat4.translate(matrix, matrix, [this.radius, 0, 0]);
 	if(this.rotAng > 0)
