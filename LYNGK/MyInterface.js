172c0ec672d45869eecbb9acdaf44e9a33a482e2
msramalho <up201403027@fe.up.pt>
2017-12-26 14:20:08 +0000

    updated MYInterface for new values

--- LYNGK/MyInterface.js
+++ LYNGK/MyInterface.js
@@ -1,13 +1,10 @@
- /**
+/**
  * MyInterface class, creating a GUI interface.
  * @constructor
  */
-function MyInterface(availableShaders) {
+function MyInterface() {
 	//call CGFinterface constructor
-	this.availableShaders = availableShaders;
-
-
-    CGFinterface.call(this);
+	CGFinterface.call(this);
 };
 
 MyInterface.prototype = Object.create(CGFinterface.prototype);
@@ -17,37 +14,49 @@ MyInterface.prototype.constructor = MyInterface;
  * Initializes the interface.
  * @param {CGFapplication} application
  */
-MyInterface.prototype.init = function(application) {
-    // call CGFinterface init
-    CGFinterface.prototype.init.call(this, application);
+MyInterface.prototype.init = function (application) {
+	// call CGFinterface init
+	CGFinterface.prototype.init.call(this, application);
 
-    // init GUI. For more information on the methods, check:
-    //  http://workshop.chromeexperiments.com/examples/gui
+	// init GUI. For more information on the methods, check:
+	//  http://workshop.chromeexperiments.com/examples/gui
 
-    this.gui = new dat.GUI();
+	this.gui = new dat.GUI();
 
+	//game folders
+	this.gameFolder = this.gui.addFolder('Game Settings');
+	this.gameFolder.open();
+	this.gameFolder.add(this.scene.server, "gameType", MyServer.gameTypes);
+	this.gameFolder.add(this.scene.server, "botLevel1", MyServer.botLevels);
+	this.gameFolder.add(this.scene.server, "botLevel2", MyServer.botLevels);
+	this.gameFolder.add(this.scene, "startNewGame");
+	// gameFolder.add(text, 'speed');
+	// gameFolder.add(text, 'noiseStrength');
 
-    return true;
+	return true;
 };
 
 /**
  * Adds a folder containing the IDs of the lights passed as parameter.
  */
-MyInterface.prototype.addLightsGroup = function(lights) {
-    var group = this.gui.addFolder("Lights");
-    group.open();
+MyInterface.prototype.addLightsGroup = function (lights) {
+	var group = this.gui.addFolder("Lights");
+	// group.open();
 
-    // add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
-    // e.g. this.option1=true; this.option2=false;
+	// add two check boxes to the group. The identifiers must be members variables of the scene initialized in scene.init as boolean
+	// e.g. this.option1=true; this.option2=false;
 
-    for (var key in lights) {
-        if (lights.hasOwnProperty(key)) {
-            this.scene.lightValues[key] = lights[key][0];
-            group.add(this.scene.lightValues, key);
-        }
-    }
+	for (var key in lights) {
+		if (lights.hasOwnProperty(key)) {
+			this.scene.lightValues[key] = lights[key][0];
+			group.add(this.scene.lightValues, key);
+		}
+	}
 };
 
+/**
+ * Adds a folder containing the IDs of the selectable nodes passed as parameter.
+ */
 /* MyInterface.prototype.addShadersGroup = function(selectables) {
     this.shadersFolder = this.gui.addFolder("Shaders Options");
 	this.shadersFolder.open();
