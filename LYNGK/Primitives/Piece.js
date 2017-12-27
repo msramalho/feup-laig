6bfd3fce3b8ff39ddaadaaa764ccc0d671f7c8ba
msramalho <up201403027@fe.up.pt>
2017-12-27 03:11:33 +0000

    piece updates to improve readability

--- LYNGK/Primitives/Piece.js
+++ LYNGK/Primitives/Piece.js
@@ -15,25 +15,35 @@ function Piece(scene, line, column, height, color) {
 	this.part5 = new Cylinder(this.scene, 0, 2, 0, 50, 50);
 	this.part6 = new Cylinder(this.scene, 0, 3.3, 0, 50, 50);
 
-	this.x = column || 0;
-	this.y = height || 0;
-	this.z = line || 0;
+	this.line = line || 0;
+	this.column = column || 0;
+	this.height = height || 0;
 	this.color = color || "noColor";
+	//for shaders
 	this.picked = false;
+	this.possible = false;
+
 	this.id = ++Piece.id;
+
+	this.scene.colorMaterial = new CGFappearance(this.scene);
+	this.scene.colorMaterial.setAmbient(0.5, 0.5, 0, 1);
+	this.scene.colorMaterial.setDiffuse(1, 0, 0, 1);
+	this.scene.colorMaterial.setSpecular(0.34, 0.32, 0.17, 1);
+	this.scene.colorMaterial.setShininess(10);
+
 }
 Piece.id = 0;
 //scale factor from prolog coordinates into the board size
 Piece.factors = {
-	z: 2.25,
+	y: 1,
 	x: 3.9,
-	y: 1
+	z: 2.25
 };
 //where does the board start
 Piece.boardStart = {
-	z: 10.5,
 	x: 9.5,
-	y: 0.5
+	y: 0.5,
+	z: 10.5
 };
 Piece.prototype = Object.create(CGFobject.prototype);
 Piece.prototype.constructor = Piece;
@@ -41,11 +51,12 @@ Piece.prototype.constructor = Piece;
 Piece.prototype.display = function () {
 	this.setColor();
 	if (this.picked) this.scene.setActiveShader(this.scene.pickedShader);
+	else if (this.possible) this.scene.setActiveShader(this.scene.possibleShader);
 	this.scene.registerForPick(this.id, this);
 	this.scene.translate(
-		Piece.factors.x * this.x + Piece.boardStart.x,
-		Piece.factors.y * this.y + Piece.boardStart.y,
-		Piece.factors.z * this.z + Piece.boardStart.z);
+		Piece.factors.x * this.column + Piece.boardStart.x,
+		Piece.factors.y * this.height + Piece.boardStart.y,
+		Piece.factors.z * this.line + Piece.boardStart.z);
 	this.scene.scale(0.45, 0.45, 0.45);
 	this.scene.rotate(Math.PI / 2, -1, 0, 0);
 
@@ -67,7 +78,7 @@ Piece.prototype.display = function () {
 	this.scene.rotate(Math.PI, 0, 1, 0);
 	this.part6.display();
 	this.scene.popMatrix();
-	if (this.picked) this.scene.setActiveShader(this.scene.defaultShader);
+	if (this.picked || this.possible) this.scene.setActiveShader(this.scene.defaultShader);
 };
 
 Piece.prototype.setColor = function () {
